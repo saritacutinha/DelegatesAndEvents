1c9fdc18eb4d5c4bb6634ecb08df63a9149d4e88
saritacutinha <37643735+saritacutinha@users.noreply.github.com>
2020-03-24 15:03:03 +1030

    Defining Event

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ namespace DelegatesAndEvents
         {
             WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
             WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
-
+            WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
+            del1 += del2 + del3;
             doWork(del1);
 
             Console.ReadLine();
@@ -28,6 +29,11 @@ namespace DelegatesAndEvents
         static void WorkPerformed2(int hours, WorkType workType)
         {
             Console.WriteLine($"Work Performed2 called "+ hours.ToString());
+        }
+
+        static void WorkPerformed3(int hours, WorkType workType)
+        {
+            Console.WriteLine($"Work Performed3 called " + hours.ToString());
         }
 
     }
---
+++ Worker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegatesAndEvents
+{
+    class Worker
+    {
+        public delegate void WorkPerformedHandler(int hours, WorkType workType);
+        public void doWork(int hours, WorkType work)
+        {
+            public event WorkPerformedHandler WorkPerformed;
+            public event EventHandler WorkCompleted;
+
+        }
+    }
+}
