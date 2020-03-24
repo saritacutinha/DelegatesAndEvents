5c326031dc16e7504bfae8971526b83042f3280b
saritacutinha <37643735+saritacutinha@users.noreply.github.com>
2020-03-24 14:18:22 +1030

    Created instances of the delegate and dump methods

--- Program.cs
+++ Program.cs
@@ -7,7 +7,19 @@ namespace DelegatesAndEvents
         public delegate void WorkPerformedHandler(int hours, WorkType workType);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+
+        }
+
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine($"Work Performed1 called");
+        }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine($"Work Performed2 called");
         }
 
     }
