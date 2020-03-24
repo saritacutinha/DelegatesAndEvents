e2d29891dd7548630f3bdb70cd20ce4de9d9a15c
saritacutinha <37643735+saritacutinha@users.noreply.github.com>
2020-03-24 14:06:36 +1030

    Created Delegate
    Created Delegate

--- Program.cs
+++ Program.cs
@@ -4,9 +4,19 @@ namespace DelegatesAndEvents
 {
     class Program
     {
+        public delegate void WorkPerformedHandler(int hours, WorkType workType);
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
     }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerareReports
+    }
+
 }
