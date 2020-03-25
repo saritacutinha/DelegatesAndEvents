709b13fd924d6b065b64505a0be953be214f5723
saritacutinha <37643735+saritacutinha@users.noreply.github.com>
2020-03-25 11:33:08 +1030

    DelegateInference

--- Program.cs
+++ Program.cs
@@ -4,33 +4,34 @@ namespace DelegatesAndEvents
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
 
             Worker worker = new Worker();
-            worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(worker_WorkPerformed);
-            worker.WorkCompleted += new EventHandler(worker_WorkCompleted);
+            worker.WorkPerformed += worker_WorkPerformed;
+            worker.WorkCompleted += worker_WorkCompleted;
             worker.DoWork(05, WorkType.GoToMeetings);
-            
+
+        }
+
+        private static void worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine($"Hours Worked-" + e.Hours + "WorkType-" + e.WorkType);
         }
 
         private static void worker_WorkCompleted(object sender, EventArgs e)
         {
             Console.WriteLine("Work Completed");
         }
+    }
 
-        private static void worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+
+        public enum WorkType
         {
-            Console.WriteLine($"Hours Worked-"+e.Hours+"WorkType-"+e.WorkType);
+            GoToMeetings,
+            Golf,
+            GenerareReports
         }
-    }
 
-    public enum WorkType
-    {
-        GoToMeetings,
-        Golf,
-        GenerareReports
     }
-
-}
