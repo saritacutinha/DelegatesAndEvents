c0f2512ec33472f46304b2c0fd9ee1b4d8c6db0a
saritacutinha <37643735+saritacutinha@users.noreply.github.com>
2020-03-25 11:34:31 +1030

    Anonymous Method

--- Program.cs
+++ Program.cs
@@ -9,17 +9,16 @@ namespace DelegatesAndEvents
         {
 
             Worker worker = new Worker();
-            worker.WorkPerformed += worker_WorkPerformed;
+            worker.WorkPerformed += delegate (object sender, WorkPerformedEventArgs e)
+                                    {
+                                        Console.WriteLine($"Hours Worked-" + e.Hours + "WorkType-" + e.WorkType);
+                                    };
             worker.WorkCompleted += worker_WorkCompleted;
             worker.DoWork(05, WorkType.GoToMeetings);
 
         }
 
-        private static void worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
-        {
-            Console.WriteLine($"Hours Worked-" + e.Hours + "WorkType-" + e.WorkType);
-        }
-
+        
         private static void worker_WorkCompleted(object sender, EventArgs e)
         {
             Console.WriteLine("Work Completed");
